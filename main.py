4e1355696680d6b1aead9695c1e3738de931c791
Zeyu Xia <i@xiaze.pro>
2023-06-01 15:43:36 +1000

    full task list

--- main.py
+++ main.py
@@ -7,10 +7,17 @@ from PIL import Image
 from tensorflow import keras
 import tensorflow as tf
 
+### Constants
+
 dataset_path = Path('./small_flower_dataset')
 pretrained_model_path = Path('mobilenetv2.h5')
 class_names = ['daisy', 'dandelion', 'roses', 'sunflowers', 'tulips']
 
+### Utils
+def get_dataset_length(dataset: tf.data.Dataset) -> int:
+    return tf.data.experimental.cardinality(dataset).numpy()
+
+### Tasks
 
 def my_team() -> 'list[tuple[int, str, str]]':
     """
@@ -23,8 +30,11 @@ def my_team() -> 'list[tuple[int, str, str]]':
     """
     return [(11262141, 'Tian-Ching', 'Lan'), (11398299, 'Zeyu', 'Xia')]
 
+
 def env_check() -> None:
     # Check if TensorFlow is built with CUDA support
+    print("*** Environment Check ***")
+
     if tf.test.is_built_with_cuda():
         print("TensorFlow is built with CUDA support.")
     else:
@@ -38,7 +48,10 @@ def env_check() -> None:
     if len(gpu_devices) == 0:
         print("No GPU available.")
 
+
 def task_1_dataset_sanity_check() -> None:
+    # Download the small flower dataset from Canvas.
+    print("*** Task 1 ***")
 
     # Check if the dataset is downloaded and extracted.
     try:
@@ -80,8 +93,10 @@ def task_1_dataset_sanity_check() -> None:
     except:
         print('At least one of the image is corrupted.')
 
-
 def task_2_download_pretrained_model() -> None:
+    # Using the tf.keras.applications module download a pretrained MobileNetV2 network.
+    print("*** Task 2 ***")
+
     if pretrained_model_path.exists():
         return
     # Using the tf.keras.applications module download a pretrained MobileNetV2 network
@@ -91,6 +106,9 @@ def task_2_download_pretrained_model() -> None:
 
 
 def task_3_replace_last_layer(classes: int = 5) -> Model:
+    # Replace the last layer of the downloaded neural network with a Dense layer of the appropriate shape for the 5 classes of the small flower dataset {(x1,t1), (x2,t2),..., (xm,tm)}.
+    print("*** Task 3 ***")
+
     model: Model = load_model(pretrained_model_path)
     print('Pretrained model loaded.')
     print('%' * 50)
@@ -106,14 +124,14 @@ def task_3_replace_last_layer(classes: int = 5) -> Model:
     print('%' * 50)
     flower_model.summary(show_trainable=True)
     print('%' * 50)
+
     return flower_model
 
 
-def get_dataset_length(dataset: tf.data.Dataset) -> int:
-    return tf.data.experimental.cardinality(dataset).numpy()
-
-
 def task_4_prepare_dataset(train_ratio=0.7, val_ratio=0.1, test_ratio=0.2, seed=42, batch_size=32):
+    # Prepare your training, validation and test sets for the non-accelerated version of transfer learning.
+    print("*** Task 4 ***")
+
     assert train_ratio + val_ratio + test_ratio == 1
 
     ds = keras.utils.image_dataset_from_directory(
@@ -158,12 +176,66 @@ def task_4_prepare_dataset(train_ratio=0.7, val_ratio=0.1, test_ratio=0.2, seed=
     return train_ds, val_ds, test_ds
 
 
+def task_5_compile_and_train(model, train_ds, val_ds, learning_rate=0.01, momentum=0, nesterov=False):
+    # Compile and train your model with an SGD optimizer using the following parameters learning_rate=0.01, momentum=0.0, nesterov=False.
+    print("*** Task 5 ***")
+
+    pass
+
+
+def task_6_plot_metrics():
+    # Plot the training and validation errors vs time as well as the training and validation accuracies.
+    print("*** Task 6 ***")
+
+    pass
+
+
+def task_7_expriment_with_different_hyperparameters():
+    # Experiment with 3 different orders of magnitude for the learning rate. Plot the results, draw conclusions.
+    print("*** Task 7 ***")
+
+    pass
+
+
+def task_8_expriment_with_different_hyperparameters():
+    # With the best learning rate that you found in the previous task, add a non zero momentum to the training with the SGD optimizer (consider 3 values for the momentum). Report how your results change.
+    print("*** Task 8 ***")
+
+    pass
+
+
+def task_9_generate_acceleated_datasets():
+    # Prepare your training, validation and test sets. Those are based on {(F(x1).t1), (F(x2),t2),...,(F(xm),tm)},
+    print("*** Task 9 ***")
+
+    pass
+
+
+def task_10_train_on_accelerated_datasets():
+    # Perform Task 8 on the new dataset created in Task 9.
+    print("*** Task 10 ***")
+    pass
+
+
 if __name__ == "__main__":
+    pass
+
     print(my_team())  # should print your team
-
+    exit()
     env_check()
-
     task_1_dataset_sanity_check()
     task_2_download_pretrained_model()
     model = task_3_replace_last_layer()
-    task_4_prepare_dataset()
+    train_ds, val_ds, test_ds = task_4_prepare_dataset()
+    task_5_compile_and_train(model, train_ds, val_ds)
+    task_6_plot_metrics()
+    task_7_expriment_with_different_hyperparameters()
+    best_learning_rate = 0.01
+    task_8_expriment_with_different_hyperparameters(best_learning_rate)
+    task_9_generate_acceleated_datasets()
+    task_10_train_on_accelerated_datasets()
+
+# TODOs:
+# Header comments
+# Each function parameter documented including type and shape of parameters
+# return values clearly documented
